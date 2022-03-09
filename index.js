78b09c32742ff327ec75758fda48d3e31202eede
Piotrko64 <PiotrPc77@gmail.com>
2022-03-09 15:45:08 +0100

    lower padding

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const { PORT, EMAIL, PASS } = process.env;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.post("/", (req, res) => {
+    const { email, body } = req.body;
 
-app.post("/:email/:body", (req, res) => {
-    const { email, body } = req.params;
-    const emailSend = email;
-    const bodySend = body;
     const date = new Date();
     const dateForHTML =
         date.getDay() +
@@ -34,11 +34,11 @@ app.post("/:email/:body", (req, res) => {
     const mailOptions = {
         from: EMAIL,
         to: EMAIL,
-        subject: "Email from" + emailSend,
+        subject: `Email from  ${email}`,
         html: `<table  style="font-family: 'Roboto'; font-size: 24px; background: #313440; width: 100%; color: white">
         <tr style="color: #C3B171; font-size: 48px; text-align: center;">NEW MESSAGE!!!</tr>
-        <tr><td  style="padding-left: 20px">From: <span style="color: #C3B171; text-align: center;">${emailSend}</span></td></tr>
-        <tr><td  style="padding-left: 20px">${bodySend}</td></tr>
+        <tr><td  style="padding-left: 10px">From: <span style="color: #C3B171; text-align: center;"><a href=mailto:${email}>${email}</a></span></td></tr>
+        <tr><td  style="padding-left: 10px">${body}</td></tr>
         <tr style="height: 60px; font-size: 14px; vertical-align: middle"><td  style="padding-left: 20px">${dateForHTML}</td></tr>
         <tr style="font-size: 16px; background: black; text-align: center; vertical-align: middle; height: 140px"> This project was made with &#x2764; by FRONT FLEX <br/><br/> <a href="https://frontflex.netlify.app"><img src="https://frontflex.netlify.app/static/minilogo-b5f69d0d94d67a6edcf46f536d48ee55.png" alt="" height=60/></a></tr>
         
@@ -52,7 +52,7 @@ app.post("/:email/:body", (req, res) => {
             console.log("Email sent: " + info.response);
         }
     });
-    res.send(body);
+    res.send(req.body);
 });
 
 app.listen(PORT, function () {
