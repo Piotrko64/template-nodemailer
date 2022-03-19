3b35223cccb219069f04113fb0922b1b31c6b178
Piotrko64 <PiotrPc77@gmail.com>
2022-03-19 23:17:17 +0100

    better padding

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ app.post("/", (req, res) => {
         <tr style="color: #C3B171; font-size: 36px; text-align: center;">NEW MESSAGE!!!</tr>
         <tr><td  style="padding-left: 5px">From: <span style="color: #C3B171; text-align: center;"><a href=mailto:${email}>${email}</a></span></td></tr>
         <tr><td  style="padding-left: 5px">${body}</td></tr>
-        <tr style="height: 60px; font-size: 12px; vertical-align: middle"><td  style="padding-left: 20px">${dateForHTML}</td></tr>
-        <tr style="font-size: 16px; background: black; text-align: center; vertical-align: middle; height: 140px"> This project was made with &#x2764; by FRONT FLEX <br/><br/> <a href="https://frontflex.netlify.app"><img src="https://frontflex.netlify.app/static/minilogo-b5f69d0d94d67a6edcf46f536d48ee55.png" alt="" height=60/></a></tr>
+         <tr style="height: 60px; font-size: 12px; vertical-align: middle"><td  style="padding-left: 5px">${dateForHTML}</td></tr>
+        <tr style="font-size: 16px; background: black; text-align: center; vertical-align: middle; height: 140px"> This project was made with &#x2764; <br/> <br/> by FRONT FLEX <br/><br/> <a href="https://frontflex.netlify.app"><img src="https://frontflex.netlify.app/static/minilogo-b5f69d0d94d67a6edcf46f536d48ee55.png" alt="" height=60/></a></tr>
         
         </table>`,
     };
