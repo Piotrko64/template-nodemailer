10a7c8015f68306a0bc8afa12b6149eedfc89bf6
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-03-19 22:53:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ app.post("/", (req, res) => {
         " " +
         date.getHours() +
         ":" +
+        (date.getMinutes() < 10 ? "0" : "") +
         date.getMinutes() +
         ":" +
+        (date.getSeconds() < 10 ? "0" : "") +
         date.getSeconds();
     const transporter = nodemailer.createTransport({
         service: "gmail",
@@ -35,11 +37,11 @@ app.post("/", (req, res) => {
         from: EMAIL,
         to: EMAIL,
         subject: `Email from  ${email}`,
-        html: `<table  style="font-family: 'Roboto'; font-size: 24px; background: #313440; width: 100%; color: white">
-        <tr style="color: #C3B171; font-size: 48px; text-align: center;">NEW MESSAGE!!!</tr>
-        <tr><td  style="padding-left: 10px">From: <span style="color: #C3B171; text-align: center;"><a href=mailto:${email}>${email}</a></span></td></tr>
-        <tr><td  style="padding-left: 10px">${body}</td></tr>
-        <tr style="height: 60px; font-size: 14px; vertical-align: middle"><td  style="padding-left: 20px">${dateForHTML}</td></tr>
+        html: `<table  style="font-family: 'Roboto'; font-size: 18px; background: #313440; width: 100%; color: white">
+        <tr style="color: #C3B171; font-size: 36px; text-align: center;">NEW MESSAGE!!!</tr>
+        <tr><td  style="padding-left: 5px">From: <span style="color: #C3B171; text-align: center;"><a href=mailto:${email}>${email}</a></span></td></tr>
+        <tr><td  style="padding-left: 5px">${body}</td></tr>
+        <tr style="height: 60px; font-size: 12px; vertical-align: middle"><td  style="padding-left: 20px">${dateForHTML}</td></tr>
         <tr style="font-size: 16px; background: black; text-align: center; vertical-align: middle; height: 140px"> This project was made with &#x2764; by FRONT FLEX <br/><br/> <a href="https://frontflex.netlify.app"><img src="https://frontflex.netlify.app/static/minilogo-b5f69d0d94d67a6edcf46f536d48ee55.png" alt="" height=60/></a></tr>
         
         </table>`,
